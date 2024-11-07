a018d846b35db233b35f0a3bd0a483e0efe4350c
chrisOmb <christopherombwori06@gmail.com>
2024-11-07 05:15:03 +0300

    assignment submitted

---
+++ assignment.py
@@ -0,0 +1,24 @@
+
+
+
+# Create an empty list called my_list.
+my_list=[]
+
+#Append the following elements to my_list: 10, 20, 30, 40.
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert the value 15 at the second position in the list.
+
+my_list.insert(1,15)
+#list 2 
+
+list_2=[50,60,70]
+
+# Append the following elements to my_list: 10, 20, 30, 40.
+
+my_list.extend(list_2)
+print(my_list)
